e5b9c078b74a68dec9cd04d13405e6d60944c7e3
user <user@user-PC>
2020-06-28 18:57:53 +0900

    12

---
+++ gajune/src/main/java/study/datastructure/Kepp.java
@@ -0,0 +1,21 @@
+package study.datastructure; 
+
+
+
+/**
+ * <pre>
+ * study.datastructure 
+ * Kepp.java
+ *
+ * 설명 :
+ * </pre>
+ * 
+ * @since : 2020. 6. 28.
+ * @author : user
+ * @version : v1.0
+ */
+public class Kepp {
+
+
+
+}
--- gajune/src/main/java/study/datastructure/queue/QueueMain.java
+++ gajune/src/main/java/study/datastructure/queue/QueueMain.java
@@ -1,4 +1,4 @@
-package study.datastructure; 
+package study.datastructure.queue; 
 
 
 
